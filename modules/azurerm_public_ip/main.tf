bff0be96527f3b9be9c1f69166a6235d57c70c7c
Hitesh Sharma <sharmah91@gmail.com>
2025-11-12 12:11:17 +0530

    public ip add

---
+++ modules/azurerm_public_ip/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_public_ip" "pip" {
+  for_each = var.public_ip_name  
+  name                = each.value.name
+  resource_group_name = each.value.rg_name
+  location            = each.value.location
+  allocation_method   = each.value.allocation_method
+
+  tags = {
+    environment = "dev"
+    project     = "todo-monolithic-infra"
+  }
+}
+
